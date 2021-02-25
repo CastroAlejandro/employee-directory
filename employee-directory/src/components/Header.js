c22196c76f34b84c415eba78abc11446c12c6717
Alejandro Castro <castroal96@gmail.com>
2021-02-24 23:57:00 -0500

    header component inserted

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div class="jumbotron jumbotron-fluid text-center bg-notification">
+			<div class="container">
+				<h1 class="display-4">Employee Directory</h1>
+				<p class="lead">This is my first react application.</p>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
