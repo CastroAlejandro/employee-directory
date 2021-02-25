2f9fec9dc326763da880c33600957c354969dc6d
Alejandro Castro <castroal96@gmail.com>
2021-02-24 23:56:11 -0500

    wrapper function created

---
+++ employee-directory/src/components/Wrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Wrapper = () => {
+	return (
+		<div className="wrapper">
+			<Header/>
+			<SearchBar/>
+			<NavBar/>
+			<Main/>
+		</div>
+	);
+}
+ 
+export default Wrapper;
