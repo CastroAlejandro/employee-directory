670c84b685891207ccf63edf08117489880daeec
Alejandro Castro <castroal96@gmail.com>
2021-02-25 22:41:29 -0500

    added map function for dynamic display of users array

--- src/Components/EmployeeTable.js
+++ src/Components/EmployeeTable.js
@@ -12,7 +12,15 @@ const EmployeeTable = (props) => {
 				</tr>
 			</thead>
 			<tbody>
-				
+			{
+					props.employees.map((e, i) => <tr key={i + "-row"}>
+						<th scope="row"><img src={e.picture.thumbnail} alt={e.name.first}/></th>
+						<td>{e.name.first} {e.name.last}</td>
+						<td>{e.phone}</td>
+						<td>{e.email}</td>
+						<td>{new Date(e.dob.date).toLocaleDateString()}</td>
+					</tr>)
+				}
 			</tbody>
 		</table>
 	);
